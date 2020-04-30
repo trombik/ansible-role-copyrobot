1884b0b2cbd329e64c45b18fc71b0f7e7ed11073
Tomoyuki Sakurai <y@trombik.org>
2020-04-30 15:31:51 +0900

    imp: initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,103 @@
 require "spec_helper"
 require "serverspec"
 
-package = "copyrobot"
-service = "copyrobot"
-config  = "/etc/copyrobot/copyrobot.conf"
-user    = "copyrobot"
-group   = "copyrobot"
-ports   = [PORTS]
-log_dir = "/var/log/copyrobot"
-db_dir  = "/var/lib/copyrobot"
 
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/copyrobot.conf"
-  db_dir = "/var/db/copyrobot"
-end
+cache_dir = "/var/cache/copyrobot"
+users = [
+  {
+    name: "_sshkey",
+    group: "_sshkey",
+    groups: ["nobody"],
+  }
+]
+shell = "/bin/sh"
+home_base_dir = case os[:family]
+           when "freebsd"
+             "/home"
+           else
+             "/usr/home"
+           end
 
-describe package(package) do
-  it { should be_installed }
-end
+users.each do |u|
+  describe group u[:group] do |g|
+    it { should exist }
+  end
 
-describe file(config) do
-  it { should be_file }
-  its(:content) { should match Regexp.escape("copyrobot") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/copyrobot") do
-    it { should be_file }
+  u[:groups].each do |g|
+    describe group g do
+      it { should exist }
+    end
   end
 end
 
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
+users.each do |u|
+  describe user u[:name] do
+    it { should exist }
+    it { should belong_to_primary_group u[:group] }
+    u[:groups].each do |g|
+      it { should belong_to_group g }
+    end
+    it { should have_home_directory "#{home_base_dir}/#{u[:name]}" }
+    it { should have_login_shell shell }
+  end
+
+  describe file "#{home_base_dir}/#{u[:name]}/.ssh" do
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by u[:name] }
+    it { should be_grouped_into u[:group] }
+    it { should be_mode 700 }
+  end
+
+  describe file "#{home_base_dir}/#{u[:name]}/.ssh/authorized_keys" do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by u[:name] }
+    it { should be_grouped_into u[:group] }
+    it { should be_mode 600 }
+  end
+
+  describe file "#{home_base_dir}/#{u[:name]}/.ssh/id_rsa" do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by u[:name] }
+    it { should be_grouped_into u[:group] }
+    it { should be_mode 600 }
+    its(:content) { should match(/^-----BEGIN (?:RSA|OPENSSH) PRIVATE KEY-----$/) }
+  end
+
+  describe file "#{home_base_dir}/#{u[:name]}/.ssh/id_rsa.pub" do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by u[:name] }
+    it { should be_grouped_into u[:group] }
+    it { should be_mode 644 }
+    its(:content) { should match(/^ssh-rsa\s+\S+\s+ansible-generated on\s+\S+$/) }
+  end
 end
 
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
+describe file cache_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 700 }
+  it { should be_owned_by "root" }
+end
+
+users.each do |u|
+  # /var/cache/copyrobot/_sshkey.secret
+  describe file "#{cache_dir}/#{u[:name]}.secret" do |f|
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 600 }
+    it { should be_owned_by "root" }
+    its(:content) { should match(/^-----BEGIN (?:RSA|OPENSSH) PRIVATE KEY-----$/) }
+  end
+
+  describe file "#{cache_dir}/#{u[:name]}.public" do |f|
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by "root" }
+    its(:content) { should match(/^ssh-rsa\s+\S+\s+ansible-generated on\s+\S+$/) }
   end
 end
